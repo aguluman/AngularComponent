afd2466d15f69784d16f1a2c1a6fd8337f344acd
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2023-08-01 14:09:14 +0100

    add: understood nested validation from planetgeek.ch

---
+++ src/app/Nested-Validation-With-ngForm/child.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core'
+import { ControlContainer, NgForm } from "@angular/forms";
+
+@Component({
+  selector: 'child',
+  template: `
+    <input type="text" [(ngModel)]="childText" name="childText" required />
+  `,
+  viewProviders: [{ provide: ControlContainer, useExisting: NgForm}],
+})
+
+export class ChildComponent {
+  childText: string;
+}
---
+++ src/app/Nested-Validation-With-ngForm/parent.component.ts
@@ -0,0 +1,16 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+  selector: 'parent',
+  template: `
+    <form #myForm="ngForm">
+      <input type="text" [(ngModel)]="parentText" name="parentText" required />
+      <child></child>
+      <button type="submit" [disabled]="!myForm.form.valid">Submit</button>
+    </form>
+  `,
+})
+
+export class ParentComponent {
+  parentText = 'foo';
+}
