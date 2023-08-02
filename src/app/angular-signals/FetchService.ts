ab9444b62ec62144d21e62541d799093c4a16ac2
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2023-08-02 14:40:42 +0100

    add: How to reuse backend result in angular signals

---
+++ src/app/angular-signals/FetchService.ts
@@ -0,0 +1,32 @@
+﻿export class FetchService {
+  async post<Tin, TOut>(path: string, value: Tin){
+    const body = JSON.stringify(value);
+    const response = await fetch(this.toUrl(path), {
+      method: 'POST',
+      body: body,
+      headers: this.getHeaders(),
+    });
+    const json = await response.json();
+    return json as TOut;
+  }
+
+  async getObject<T>(path: string) {
+    const response = await fetch(this.toUrl(path), {
+        method: 'GET',
+        headers: this.getHeaders(),
+      });
+    const json = await response.json();
+    return json as T;
+  }
+
+  private  toUrl = (path: string) =>
+    `https://jsonplaceholder.typicode.com${path}`
+
+  private getHeaders(): HeadersInit {
+    const token = '<someToken>';
+    return {
+      Authorization: `Bearer ${token}`,
+      'Content-type': 'application/json; charset=UTF-8',
+    };
+  }
+}
---
+++ src/app/angular-signals/TodoService.ts
@@ -0,0 +1,21 @@
+﻿import {SignalService} from "./SignalService";
+import {Signal} from "@angular/core";
+
+export class TodoService extends SignalService {
+  getTodos(): Signal<Todo> {
+    return  this.getObject<Todo>('/todos');
+  }
+
+  async addTodo(todo: Todo) {
+    const result = await this.fetchService.post('/todos', todo);
+    this.contentChanged();
+    return result;
+  }
+}
+
+export interface Todo {
+  userId: number;
+  id?: number;
+  title: string;
+  completed: boolean;
+}
