913e26d19181fb22c47eece6ce36a160fc1f84e1
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2023-11-17 06:49:34 +0100

    Add TodoUpdateAction and TodosView components, update .gitignore and package.json
    Added TodoUpdateAction and TodosView components for handling the operations related to updating and viewing todos respectively. Ignore package-lock.json in .gitignore to prevent conflicts from different npm versions. Added 'ts-evaluator' dependency in package.json to assist in type checking of Typescript.

---
+++ src/app/angular-signals/todoUpdate.action.ts
@@ -0,0 +1,40 @@
+﻿import {ChangeDetectionStrategy, Component, effect} from "@angular/core";
+import {ActivatedRoute} from "@angular/router";
+import {Todo, TodoService} from "./TodoService";
+import {toSignal} from "@angular/core/rxjs-interop";
+
+@Component({
+  selector: 'update-action',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <input type="text" [(ngModel)]="title" name="title"/>
+    <input type="checkbox" [(ngModel)]="completed" name="completed"/>
+    <button (click)="edit()">Edit</button>
+  `
+})
+
+export class TodoUpdateAction {
+  id: number;
+  title: string;
+  completed: boolean;
+
+  constructor(
+    activatedRoute: ActivatedRoute,
+    private todoService: TodoService
+  ) {
+    const params = toSignal(activatedRoute.params);
+    effect(() => {
+      const todo = decide<Todo>(params()?.data);
+      this.id = todo.id;
+      this.title = todo.title
+      this.completed = todo.completed;
+    });
+  }
+
+  async edit() {
+    await this.todoService.updateTodo(this.id, {
+      title: this.title,
+      completed: this.completed,
+    });
+  }
+}
---
+++ src/app/angular-signals/view.component.ts
@@ -0,0 +1,72 @@
+﻿import {ChangeDetectionStrategy, ChangeDetectorRef, Component, computed, effect, signal} from "@angular/core";
+import {Todo, TodoService} from "./TodoService";
+
+@Component({
+  selector: 'todos-views',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <div *ngIf="!isLoading; else loadingIndicator" class="main">
+      {{ todos() | json }}
+      <button type="button" (click)="reload()">Reload</button>
+    </div>
+    <ng-template #loadingIndicator>loading...</ng-template>
+  `,
+})
+
+export class TodosView {
+  constructor(private todoService: TodoService,
+              private userService: UserService) {
+    effect(
+      () => {
+        const _ = this.trigger();
+        this.isLoading.set(true);
+        this.todoService.contentChanged();
+        this.userService.contentChanged();
+      },
+      {allowSignalWrites: true}
+    );
+
+    effect(
+      () => {
+        const todos = this.todoService.getTodos()();
+        const users = this.userService.getAllUsers()();
+
+        if (!todos || !users) {
+          this.isLoading.set(false);
+          this.todos.set([]);
+          return;
+        }
+
+        const result = todos.map<AugmentedTodo>((t) => {
+          const user = users.find((u) => u.id === t.userId)!;
+          return {
+            id: t.id!,
+            completed: t.completed,
+            title: t.title,
+            username: user.username,
+          };
+        })
+          .slice(0, 10);
+
+        this.isLoading = false;
+        this.todos.set(result);
+      },
+      { allowSignalWrites: true}
+    );
+  }
+
+  todos: Signal<AugmentedTodo[]>([]);
+  trigger = signal(createGuid());
+  isLoading = signal(true);
+
+  reload() {
+    this.trigger.set(createGuid())
+  }
+}
+
+export interface AugmentedTodo {
+  id: number;
+  username: string;
+  title: string;
+  completed: boolean
+}
